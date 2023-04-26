ff2de9fac9ca812d837b33dafb887f77279c6bd2
Amogh Parmeswaran <amogh_parmeswaran@brown.edu>
2023-04-25 20:06:34 -0400

    feat: Created encoder

---
+++ main.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from transformers import BertTokenizer, BertModel
+
+def encoder(article_text):
+    # Load a pretrained BERT tokenizer and model
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    model = BertModel.from_pretrained("bert-base-uncased")
+
+    # Tokenize the input text and convert to input IDs
+    input_ids = tokenizer.encode(article_text, add_special_tokens=True)
+    input_ids = tf.constant([input_ids])
+
+    # Forward pass through the BERT model to obtain embeddings
+    outputs = model(input_ids)
+    embeddings = outputs.last_hidden_state[0]
+
+    return embeddings.numpy()
+
+
+

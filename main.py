cbd64eb7418978da3f73ca95afa155eb2febe47c
Amogh Parmeswaran <amogh_parmeswaran@brown.edu>
2023-04-27 15:11:17 -0400

    feat: Finished encoding

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import tensorflow as tf
 import tensorflow_hub as hub
-import tensorflow_text as text
-
+import tensorflow_text as text 
 
 def encode_text(article_text):
-    article_text = tf.constant([article_text])
+    # NOTE: The following code on creating the encoder was taken from
+    # https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/4
+    # which details basic usage of creating a model to encode text
+    # using BERT. 
     text_input = tf.keras.layers.Input(shape=(), dtype=tf.string)
     preprocessor = hub.KerasLayer(
         "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
     encoder_inputs = preprocessor(text_input)
     encoder = hub.KerasLayer(
-        "https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/3",
+        "https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/4",
         trainable=True)
     outputs = encoder(encoder_inputs)
-    
+    # Use pooled_output since we want to represent each input sequence as a whole
+    # which will be useful for classification of the article text 
+    pooled_output = outputs["pooled_output"]
     # Create a Keras model to extract the encoded features
-    model = tf.keras.Model(inputs=text_input, outputs=outputs)
+    embedding_model = tf.keras.Model(text_input, pooled_output)
     
     # Encode the text
-    encoded_text = model(article_text)
+    article_text = tf.constant([article_text])
+    return embedding_model(article_text)
     
-    return encoded_text
 
 if __name__ == '__main__':
     print(4)
-    print(encode_text("Hello World I want to shoot this computer so badly holy fuck"))
+    print(encode_text("Hello World"))

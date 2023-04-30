ed941e3bc5247086b68835718151c6a6e9cf201d
Amogh Parmeswaran <amogh_parmeswaran@brown.edu>
2023-04-30 11:37:59 -0400

    feat: Forward pass of train works, accuracy shapes still mismatched

--- main.py
+++ main.py
@@ -1,52 +1,63 @@
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text 
-import numpy as np
-from encoder import encode_text
 from sklearn.model_selection import train_test_split
 import pandas as pd
+from encoder import encode_text
 from LinearModel import LinearModel
 
 publishers = ["CNBC", "CNN", "Economist", "Fox News", "People", "The New York Times", "People", "Vice News", "Politico", "Reuters", "TMZ"]
-class ModelTrainer():
-    def __init__(self, model, train_data, test_data, num_classes):
-        self.train_data = train_data
-        self.test_data = test_data
-        self.num_classes = num_classes
-        self.model = model(num_classes)
-    
-    def train(self, epochs, batch_size, learning_rate):
-        # Split the training data into input and target arrays
-        (input_train, label_train) = self.train_data
-        
-        # Compile the model with categorical cross-entropy loss and Adam optimizer
-        self.model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(learning_rate), metrics = ['accuracy'])
-        
-        # Fit the model to the training data
-        self.model.fit(input_train, label_train, epochs = epochs, batch_size = batch_size, verbose=3)
-    
-    def test(self):
-        # Split the testing data into input and target arrays
-        (input_test, label_test) = self.test_data
-        
-        # Encode the text inputs and predict the class labels
-        encoded_text_array = []
-        for article_text in input_test:
-            encoded_text = encode_text(article_text)
-            encoded_text_array.append(encoded_text)
-        encoded_text_array = np.array(encoded_text_array)
 
-        label_predictions = np.argmax(self.model(encoded_text_array), axis = 1)
-        
-        # Calculate the test accuracy
-        accuracy = np.mean(label_predictions == label_test)
-        print(f"Test Accuracy: {accuracy}")
+class ModelTrainer():
+    def __init__(self, model, train_inputs, train_labels, test_inputs, test_labels, num_classes):
+        self.train_inputs = train_inputs
+        self.train_labels = train_labels
+        self.test_inputs = test_inputs
+        self.test_labels = test_labels
+        self.model = model(num_classes)
+
+    def train(self, epochs, batch_size):
+        optimizer = tf.keras.optimizers.Adam()
+        loss_fn = tf.keras.losses.CategoricalCrossentropy()
+
+        for epoch in range(epochs):
+            print("Epoch: {}".format(epoch + 1))
+            # Shuffle the training data for each epoch
+            indices = tf.range(start=0, limit=tf.shape(self.train_inputs)[0], dtype=tf.int32)
+            shuffled_indices = tf.random.shuffle(indices)
+            train_inputs = tf.gather(self.train_inputs, shuffled_indices)
+            train_labels = tf.gather(self.train_labels, shuffled_indices)
+
+            for batch in range(0, len(train_inputs), batch_size):
+                inputs = train_inputs[batch : batch + batch_size]
+                labels = train_labels[batch : batch + batch_size]
+
+                with tf.GradientTape() as tape:
+                    predictions = self.model(inputs)
+                    loss = loss_fn(labels, predictions)
+
+                gradients = tape.gradient(loss, self.model.trainable_variables)
+                optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+
+            train_accuracy = self.evaluate(self.train_inputs, self.train_labels)
+            print("Training accuracy: {:.2f}%".format(train_accuracy * 100))
+
+    def test(self):
+        test_accuracy = self.evaluate(self.test_inputs, self.test_labels)
+        print("Test accuracy: {:.2f}%".format(test_accuracy * 100))
+
+    def evaluate(self, inputs, labels):
+        predictions = self.model(inputs)
+        correct_predictions = tf.equal(tf.argmax(predictions, axis=1), tf.argmax(labels, axis=1))
+        accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+        return accuracy.numpy()
+
 
 def process_data(filepath):
     data = pd.read_csv(filepath)
     # Get all the articles and publications from the CSV of aggregate data
-    article_data = np.array(data['article'])[:10]
-    publication_data = np.array(data['publication'])[:10]
+    article_data = list(data['article'])[:10]
+    publication_data = list(data['publication'])[:10]
 
     # Replacing publisher with classifier index
     for i in range(len(publication_data)):
@@ -60,18 +71,20 @@ def process_data(filepath):
     for index, article in enumerate(article_data):
         article_data[index] = encode_text(article, preprocessor, encoder)
         print("Encoded: ", index)
+        print(article_data[index])
 
     # Split article data and publication labels into training and testing sets
-    input_train, input_test, label_train, label_test = train_test_split(article_data, publication_data, train_size = 0.8, random_state = 100)
+    train_inputs, test_inputs, train_labels, test_labels = train_test_split(article_data, publication_data, train_size = 0.8, random_state = 100)
+
 
     # One hot encode publication labels
-    label_train, label_test = tf.one_hot(label_train, 10), tf.one_hot(label_test, 10)
+    train_labels, test_labels = tf.one_hot(train_labels, 10), tf.one_hot(test_labels, 10)
 
-    return np.array(input_train), np.array(input_test), label_train, label_test
+    return train_inputs, test_inputs, train_labels, test_labels
 
 if __name__ == '__main__':
-    input_train, input_test, label_train, label_test = process_data('Samples/samples.csv')
-    modelTrainer = ModelTrainer(LinearModel, (input_train, label_train), (input_test, label_test), 10)
-    modelTrainer.train(10, 32, 0.003)
+    train_inputs, test_inputs, train_labels, test_labels = process_data('Samples/random.csv')
+    modelTrainer = ModelTrainer(LinearModel, train_inputs, train_labels, test_inputs, test_labels, 10)
+    modelTrainer.train(10, 32)
     modelTrainer.test()
 
